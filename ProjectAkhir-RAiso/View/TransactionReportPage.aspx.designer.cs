8b76ee9586f1cca9f71825292648b1ad441f6627
Ryldi <reynaldi.adidarma@binus.ac.id>
2024-06-08 01:05:52 +0700

    style: adding back-button in Transaction Report Page

--- ProjectAkhir-RAiso/View/TransactionReportPage.aspx.designer.cs
+++ ProjectAkhir-RAiso/View/TransactionReportPage.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace ProjectAkhir_RAiso.View
     {
 
         /// <summary>
+        /// Btn_Back control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton Btn_Back;
+
+        /// <summary>
         /// CR_Viewer control.
         /// </summary>
         /// <remarks>
